e43802e03a0a55a8a6e95e3a1ad4b661bc6d5b56
ceylonsystems <ceylonsystems@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-16 09:32:40 +0000

    release 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/igniteup/tags/1.1.1@1199947 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/launcher.php
@@ -0,0 +1,161 @@
+<?php
+
+global $cscs_templates;
+$cscs_templates['launcher'] = array(
+    'name' => 'Launcher',
+    'folder_name' => 'launcher',
+    'options' => array(
+        'launch_date' => array(
+            'type' => 'date',
+            'label' => 'Launch Date',
+            'placeholder' => 'mm/dd/yyyy',
+            'def' => date('m/d/Y', strtotime('Next Monday')),
+            'description' => 'Add the date when you are going to launch the site',
+        ),
+        'launch_time' => array(
+            'type' => 'text',
+            'label' => 'Launch Time',
+            'placeholder' => 'hh:mm:ss',
+            'def' => '12:12:12',
+            'description' => 'Note: Enter time in hh:mm:ss format.',
+        ),
+        'bg_color' => array(
+            'type' => 'color-picker',
+            'label' => 'Background Color',
+            'def' => '#28BB9B',
+            'placeholder' => '#28BB9B',
+            'description' => 'This will be the background color.',
+        ),
+        'bg_image' => array(
+            'type' => 'image',
+            'label' => 'Background Image',
+            'def' => '',
+            'placeholder' => '',
+            'description' => 'Page background image. (Recommended size: 1920px x 1080px)',
+        ),
+        'font_color' => array(
+            'type' => 'color-picker',
+            'label' => 'Font Color',
+            'def' => '#fff',
+            'placeholder' => '#FFFFFF',
+            'description' => 'This will be the font color',
+        ),
+        'title_top' => array(
+            'type' => 'text',
+            'label' => 'Title Top Text',
+            'def' => 'Almost Ready',
+            'placeholder' => 'Bold Title',
+            'description' => 'This will be the bold title',
+        ),
+        'title' => array(
+            'type' => 'text',
+            'label' => 'Subtitle Text',
+            'def' => 'Website will launch in',
+            'placeholder' => 'Subtitle',
+            'description' => 'Text below the title',
+        ),
+        'paragraph' => array(
+            'type' => 'textarea',
+            'label' => 'Paragraph Text',
+            'def' => 'This website is currently unavailable due to maintenance. Please visit again later. If you have any inquiries forward to the site admin. Please subscribe with our Newsletter.',
+            'placeholder' => 'Paragraph Text',
+            'description' => 'This will be the paragraph text',
+        ),
+        'subscribe_text' => array(
+            'type' => 'text',
+            'label' => 'Button Text',
+            'def' => 'Subscribe',
+            'placeholder' => 'Subscribe',
+            'description' => 'Max: 15 Characters',
+        ),
+        'subscribe_text_color' => array(
+            'type' => 'color-picker',
+            'label' => 'Button Font Color',
+            'def' => '#fff',
+            'placeholder' => '#FFFFFF',
+            'description' => 'This will be the font color the button',
+        ),
+        'subscribe_bg_color' => array(
+            'type' => 'color-picker',
+            'label' => 'Button Background Color',
+            'def' => '#DB4F4B',
+            'placeholder' => '#FFFFFF',
+            'description' => 'This will be the background color the button',
+        ),
+        'social-twitter' => array(
+            'type' => 'text',
+            'label' => 'Twitter',
+            'def' => '',
+            'placeholder' => 'http://twitter.com/ceylonsystems',
+            'description' => 'Enter the Twitter URL here',
+        ),
+        'social-facebook' => array(
+            'type' => 'text',
+            'label' => 'Facebook',
+            'def' => '',
+            'placeholder' => 'http://facebook.com/ceylonsystems',
+            'description' => 'Enter the Facebook URL here',
+        ),
+        'social-pinterest' => array(
+            'type' => 'text',
+            'label' => 'Pinterest',
+            'def' => '',
+            'placeholder' => 'http://pinterest.com/ceylonsystems',
+            'description' => 'Enter the Pinterest URL here',
+        ),
+        'social-gplus' => array(
+            'type' => 'text',
+            'label' => 'Google+',
+            'def' => '',
+            'placeholder' => 'http://plus.google.com/+ceylonsystems',
+            'description' => 'Enter the Google+ URL here',
+        ),
+        'social-youtube' => array(
+            'type' => 'text',
+            'label' => 'Youtube',
+            'def' => '',
+            'placeholder' => 'http://youtube.com/ceylonsystems',
+            'description' => 'Enter the Youtube URL here',
+        ),
+        'social-evernote' => array(
+            'type' => 'text',
+            'label' => 'Evernote',
+            'def' => '',
+            'placeholder' => 'http://evernote.com/ceylonsystems',
+            'description' => 'Enter the Evernote URL here',
+        ),
+        'social-tumblr' => array(
+            'type' => 'text',
+            'label' => 'Tumblr',
+            'def' => '',
+            'placeholder' => 'http://tumblr.com/ceylonsystems',
+            'description' => 'Enter the Tumblr URL here',
+        ),
+        'social-behance' => array(
+            'type' => 'text',
+            'label' => 'Behance',
+            'def' => '',
+            'placeholder' => 'http://behance.net/ceylonsystems',
+            'description' => 'Enter the Behance URL here',
+        ),
+        'social-linkedin' => array(
+            'type' => 'text',
+            'label' => 'Linkedin',
+            'def' => '',
+            'placeholder' => 'http://linkedin.com/ceylonsystems',
+            'description' => 'Enter the Linkedin URL here',
+        ),
+    )
+);
+
+function cscs_launcher_theme_scripts(){
+  wp_enqueue_style('bootstrap', plugins_url('includes/css/bootstrap.min.css', CSCS_FILE));  
+  wp_enqueue_style('animate', plugins_url('includes/css/animate.css', CSCS_FILE));
+  wp_enqueue_style('font-montserrat', plugins_url('includes/css/font-montserrat.css', CSCS_FILE));
+  wp_enqueue_style('launcher', plugins_url('launcher/css/main.css', __FILE__));
+  wp_enqueue_style('launcher-icons', plugins_url('includes/css/icons/styles.css', CSCS_FILE));
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('jquery-countdown', plugins_url('launcher/js/jquery.countdown.js', __FILE__),  array('jquery'));
+}
+
+add_action('cscs_theme_scripts_launcher', 'cscs_launcher_theme_scripts');
---
+++ readme.txt
@@ -0,0 +1,124 @@
+=== IgniteUp - Coming Soon and Maintenance Mode ===
+Contributors: ceylonsystems
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=E6MY6RKB8DAEL
+Tags: coming soon, coming soon page, construction, landing page, launch, launch page, maintenance, maintenance mode, newsletter, subscribe, offline, unavailable, under construction, wordpress coming soon, wordpress maintenance mode, wordpress under construction, igniter
+Requires at least: 3.0.1
+Tested up to: 4.2.2
+Stable tag: 1.1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Display Coming Soon, Under Constructions & Maintenance Mode pages for your site visitors.
+
+== Description ==
+
+Create decent pages to let users know the site is in Coming Soon, Maintenance Mode or Under Construction mode. IgniteUp comes with simple but more customizable templates. You can change everything in the theme according to your needs.
+
+= Live Demo Pages = 
+
+* [Live Demo 1 &raquo;](http://plugins.ceylonsystems.com/igniteup-demo-1/)
+* [Live Demo 2 &raquo;](http://plugins.ceylonsystems.com/igniteup-demo-2/)
+* [Live Demo 3 &raquo;](http://plugins.ceylonsystems.com/igniteup-demo-3/)
+* [Live Demo 4 &raquo;](http://plugins.ceylonsystems.com/igniteup-demo-4/)
+* [Live Demo 5 &raquo;](http://plugins.ceylonsystems.com/igniteup-demo-5/)
+
+= Features =
+
+* Active Support for Your Questions 
+* Supporting every WordPress theme.
+* 100% Mobile Responsive
+* Powered by Twitter Bootstrap
+* Fully Customizable Templates
+* Email Subscription Form Available in Launcher Template
+* Export Email Subscriber List as a CSV or BCC Text
+* Translation Ready
+* Multisite Support
+* More New Templates Coming Soon with Future Updates
+
+= Included Languages =
+
+* Sinhala (si_LK)
+
+== Installation ==
+
+= Automatic Installation =
+
+1. In the dashboard navigate to `Plugins > Add New`
+2. Search for `IgniteUp`
+3. Locate the `IgniteUp` by `Ceylon Systems` and click `Install Now`
+4. After installation, click on `Activate Plugin` to activate
+
+= Manual Installation =
+
+1. Download the zip file using the `Download` button above in this page
+2. In the dashboard navigate to `Plugins > Add New`
+3. Click on `Upload Plugin`
+4. Click `Choose File` then select the zip file you downloaded
+5. Click `Install Now`, wait for the installation and click `Activate Plugin`
+
+= Enabling the Landing Page =
+
+1. In the dashboard, navigate to `IgniteUp > Options`
+2. Tick the checkbox for `Enable/Disable` option
+3. Save the setting by clicking the `Save` button. 
+
+1, 2, 3: You're done!
+
+== Frequently Asked Questions ==
+
+= How to enable maintanance / coming soon mode? =
+Navigate to plugin's options page (`IgniteUp > Options`) and tick `Enable / Disable` checkbox. That's all!
+
+= How to switch templates? =
+Navigate to templates page (`IgniteUp > Templates`) and click on the `Activate` button in the appropriate template you want to activate. Active template is shown in the first place.
+
+= How to customize template options? =
+Navigate to `IgniteUp > Options` and click the tab `Template Options`. There you can find all the options for the activated template.
+
+= Why can't I see IgniteUp page? =
+
+Make sure the plugin is activated. And also you should enable the option `Enable/Disable` in the IgniteUp Options (`Ignite > Options`) page. Note: IgniteUp page won't be seen by users who are logged in and belongs to the user roles ticked on `Skip Page For` option.
+
+= How can I reach IgniteUp developers for help? =
+
+You can post your question / suggestion in the [IgniteUp forums](https://wordpress.org/support/plugin/igniteup). We will reach you as soon as possible. You can also email us via support[at]ceylonsystems[dot]com for any support requests.
+
+== Screenshots ==
+
+1. Believe Template on Default Options
+2. Launcher Template on Default Options
+3. Offline Template on Default Options
+4. Glass Template on Default Options
+5. Believe Template Modified with Background Image
+
+== Changelog ==
+
+= 1.1.1 = 
+* Fix a bug occurred on network installations
+
+= 1.1 =
+* Add new template GLASS
+* Improved to dequeue other template stylesheets when page is enabled
+* Template page improved to be mobile responsive
+
+= 1.0.5 =
+* Fix mobile responsive issues
+* Fix few minor bugs
+
+= 1.0.4 =
+* Improve template mobile responsiveness
+
+= 1.0.3 =
+* Remove skip for admin from default options
+* Fixed minor bugs
+
+= 1.0.2 =
+* Automatic force for black background-color when background image selected
+* Few performance improvements
+* Fixed known theme conflicts
+
+= 1.0.1 =
+* Fixed title not showing for empty values
+
+= 1.0 =
+* Initial Release
